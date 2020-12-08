14e99762f38d166cc62ef9b542287678a19ce44c
Riccardo Gulin <riccardo@nucleode.com>
2020-12-08 12:50:09 +0100

    Added comments

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 const express = require("express")
 const cors = require("cors")
-
-const users = require("./services/users")
+const usersRoutes = require("./users")
 
 const server = express()
 
-server.use(cors())
-server.use(express.json())
+const port = 3001
 
-server.use("/users", users)
-server.listen(3001, () => {
-  console.log("Server running on port 3001")
+server.use(cors())
+server.use(express.json()) // I need to specify this line of code otherwise all the request bodies will be undefined. And this line of code must come BEFORE the routes
+
+server.use("/users", usersRoutes)
+
+server.listen(port, () => {
+  console.log("Server is running on port: ", port)
 })
--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,58 +1,139 @@
-const express = require("express")
-const fs = require("fs")
-const path = require("path")
-const uniqid = require("uniqid")
+/*
+
+1. get all users on url --> localhost:3001/users/
+2. get single user on url --> localhost:3001/users/:id
+3. create a single user --> localhost:3001/users
+4. modify a single user --> localhost:3001/users/:id
+5. delete a single user --> localhost:3001/users/:id
+
+All the routes in this file will have the /users/ prefix
+*/
+
+const express = require("express") // third party module
+const fs = require("fs") // core module
+const path = require("path") // core module
+const uniqid = require("uniqid") // third party module
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-  const buffer = fs.readFileSync(path.join(__dirname, "users.json"))
-  const fileContent = buffer.toString()
+// 1.router.get("/")
 
-  res.send(JSON.parse(fileContent))
+router.get("/", (req, res) => {
+  // handler
+  // console.log(__dirname) // current directory
+  // console.log(path.join(__dirname, "users.json")) // this is the correct way to concatenate two paths
+  // console.log(__dirname + "\\users.json") // Do not concatenate paths in this way, use path.join()
+  const usersFilePath = path.join(__dirname, "users.json")
+  // a) Retrieve the list from a file on disk (users.json), because we don't have a real database yet!
+  const fileAsABuffer = fs.readFileSync(usersFilePath) // returns a buffer (machine readable, not human readable)!!!
+  // b) we get a buffer so this must be converted to something readable (like a String)
+  const fileAsAString = fileAsABuffer.toString()
+  // c) I want to send the list as a response but in the form of a JSON (not a String)
+
+  const usersArray = JSON.parse(fileAsAString)
+
+  // d) Now I can finally send it as a response
+  res.send(usersArray)
 })
 
-router.get("/:id", (req, res) => {
-  const buffer = fs.readFileSync(path.join(__dirname, "users.json"))
-  const usersArray = JSON.parse(buffer.toString())
-  const user = usersArray.filter(user => user.ID === req.params.id)
+// 2. router.get("/:id")
 
+router.get("/:identifier", (req, res) => {
+  // handler
+
+  // 1. read the content from the file
+  const usersFilePath = path.join(__dirname, "users.json")
+  const fileAsABuffer = fs.readFileSync(usersFilePath)
+  const fileAsAString = fileAsABuffer.toString()
+  const usersArray = JSON.parse(fileAsAString)
+
+  // 2. grab the id from the url
+  const idComingFromRequest = req.params.identifier
+  console.log("---------------->", idComingFromRequest)
+
+  // 3. filter the array searching for that id
+  const user = usersArray.filter(user => user.ID === idComingFromRequest)
+  console.log("USER ", user)
+
+  // 4. send the found user back as a response
   res.send(user)
 })
 
+// 3. router.post("/")
+
 router.post("/", (req, res) => {
+  // handler
+
+  // 1. read the old content from the file
+  const usersFilePath = path.join(__dirname, "users.json")
+  const fileAsABuffer = fs.readFileSync(usersFilePath)
+  const fileAsAString = fileAsABuffer.toString()
+  const usersArray = JSON.parse(fileAsAString)
+
+  // 2. push new user to usersArray
+
+  // 2.1 let's create a unique identifier for him
+
   const newUser = req.body
-  const buffer = fs.readFileSync(path.join(__dirname, "users.json"))
-  const content = buffer.toString()
-  const usersDB = JSON.parse(content)
   newUser.ID = uniqid()
-  usersDB.push(newUser)
-  fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(usersDB))
+  console.log(newUser)
+  usersArray.push(newUser)
+  console.log(usersArray)
+
+  // 3. replace old content in the file with new array
+
+  fs.writeFileSync(usersFilePath, JSON.stringify(usersArray))
 
   res.status(201).send({ id: newUser.ID })
 })
 
-router.delete("/:id", (req, res) => {
-  const buffer = fs.readFileSync(path.join(__dirname, "users.json"))
-  const content = buffer.toString()
-  const usersDB = JSON.parse(content)
-  const newDb = usersDB.filter(x => x.ID !== req.params.id)
-  fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(newDb))
-
-  res.status(204).send()
-})
+// 4. router.put("/:id")
 
 router.put("/:id", (req, res) => {
-  const buffer = fs.readFileSync(path.join(__dirname, "users.json"))
-  const content = buffer.toString()
-  const usersDB = JSON.parse(content)
-  const newDb = usersDB.filter(x => x.ID !== req.params.id)
-  const users = req.body
-  users.ID = req.params.id
-  newDb.push(users)
-  fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(newDb))
+  // handler
 
-  res.send(newDb)
+  // 1. Read the file
+  const usersFilePath = path.join(__dirname, "users.json")
+  const fileAsABuffer = fs.readFileSync(usersFilePath)
+  const fileAsAString = fileAsABuffer.toString()
+  const usersArray = JSON.parse(fileAsAString)
+
+  // 2. Filter out the user with the specified id
+  const newUsersArray = usersArray.filter(user => user.ID !== req.params.id)
+
+  // 3. Create the modifiedUser object taking data from the request body
+
+  const modifiedUser = req.body
+  modifiedUser.ID = req.params.id
+
+  // 4. Add the modified user back to the array
+  newUsersArray.push(modifiedUser)
+
+  // 5. Write it back on disk
+
+  fs.writeFileSync(usersFilePath, JSON.stringify(newUsersArray))
+  res.send("Modify user route")
+})
+
+// 5. router.delete("/:id")
+
+router.delete("/:id", (req, res) => {
+  // handler
+
+  // 1. Read the file
+  const usersFilePath = path.join(__dirname, "users.json")
+  const fileAsABuffer = fs.readFileSync(usersFilePath)
+  const fileAsAString = fileAsABuffer.toString()
+  const usersArray = JSON.parse(fileAsAString)
+
+  // 2. Filter out the user with the specified id
+  const newUsersArray = usersArray.filter(user => user.ID !== req.params.id)
+
+  // 3. Save it back on disk
+
+  fs.writeFileSync(usersFilePath, JSON.stringify(newUsersArray))
+
+  res.status(204).send()
 })
 
 module.exports = router
