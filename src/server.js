e818b542b47845ba8a16677979f7290a1731decb
Riccardo Gulin <riccardo@nucleode.com>
2020-12-08 14:43:59 +0100

    Fixed path

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-const usersRoutes = require("./users")
+const usersRoutes = require("./services/users")
 
 const server = express()
 
